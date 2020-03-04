63183b0c0ee3c8838da66c8e4222559cfeb62da7
Zachary Velyvis <zv01282@srcs.loc>
2020-03-04 14:43:54 -0800

    create grid function

---
+++ js.js
@@ -0,0 +1,13 @@
+function createGrid() {
+    var grid = document.getElementById("grid");
+
+    for(var i = 0; i < 50; i++) {
+        for(var j = 0; j < 50; j++) {
+            var box = document.createElement("box");
+            box.setAttribute("class", "unit");
+            box.style.left = i*16+"px";
+            box.style.top = j*16+"px";
+            grid.appendChild(box);
+        }
+    }
+}
