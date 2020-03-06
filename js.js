a0f465eb526162bcf001e121e4c8bd69cce4e5cc
Zachary Velyvis <zv01282@srcs.loc>
2020-03-06 14:15:03 -0800

    ty mr temple

--- js.js
+++ js.js
@@ -5,23 +5,12 @@ function createGrid() {
         for(var j = 0; j < 50; j++) {
             var box = document.createElement("box");
             box.setAttribute("class", "unit");
-            box.setAttribute("id", i+","+j)
             box.style.left = i*16+"px";
             box.style.top = j*16+"px";
-            grid.appendChild(box);
-        }
-    }
-
-    overUnit();
-}
-
-function overUnit() {
-    for(var i = 0; i < 50; i++) {
-        for(var j = 0; j < 50; j++) {
-            var unit = document.getElementById(i+","+j);
-            unit.addEventListener('mouseover', (event) => {
+            box.addEventListener('mouseover', (event) => {
                 changeColor(event);
             })
+            grid.appendChild(box);
         }
     }
 }
@@ -29,5 +18,4 @@ function overUnit() {
 function changeColor(event) {
     let cell = event.target;
     cell.style.background = "red";
-    overUnit();
 }
