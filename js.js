4fe1704b96fbc3976457081969cc8410ba434008
Zachary Velyvis <zv01282@srcs.loc>
2020-03-06 13:52:35 -0800

    interactivity test1

--- js.js
+++ js.js
@@ -5,9 +5,29 @@ function createGrid() {
         for(var j = 0; j < 50; j++) {
             var box = document.createElement("box");
             box.setAttribute("class", "unit");
+            box.setAttribute("id", i+","+j)
             box.style.left = i*16+"px";
             box.style.top = j*16+"px";
             grid.appendChild(box);
         }
     }
+
+    overUnit();
+}
+
+function overUnit() {
+    for(var i = 0; i < 50; i++) {
+        for(var j = 0; j < 50; j++) {
+            var unit = document.getElementById(i+","+j);
+            unit.addEventListener('mouseover', (event) => {
+                changeColor(event);
+            })
+        }
+    }
+}
+
+function changeColor(event) {
+    let cell = event.target;
+    cell.style.background = "red";
+    overUnit();
 }
